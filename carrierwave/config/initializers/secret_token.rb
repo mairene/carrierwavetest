2bfb22fd5af4541340d7d2c9897d7153925087a7
Irene Ma <irene.ma@irenema-ltm.internal.salesforce.com>
2016-01-08 09:54:42 -0800

    initial commit

---
+++ carrierwave/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carrierwave::Application.config.secret_token = '1e373013a99900f8afd7263446db5eac87d2af7ae4c075a43c8d49407cc8ee0987b27fc4699127bb44cb58859f8cee8c46a4851b7e91483e7b8afca6e4ef24b1'
